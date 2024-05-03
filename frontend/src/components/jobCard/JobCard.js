1777dd6b2bc9a4187bef16e8b1f620ffab85a0e2
Nishchaya Narula <n.nishchaya2000@gmail.com>
2024-05-03 16:33:43 +0530

    integrated api and check data values

--- frontend/src/components/jobCard/JobCard.js
+++ frontend/src/components/jobCard/JobCard.js
@@ -1,25 +1,42 @@
 import React from 'react'
 import "./JobCard.css"
 
-const JobCard = () => {
+const JobCard = ({ jobData_items }) => {
     return (
         <div className='card'>
             <div className="card_details">
                 <div className='card_details-post'>Posted 6 days ago</div>
-                <div className="card_details-companyName">DeGenerous</div>
-                <div className='card_details-jobTitle'>Software Productivity Engineer</div>
-                <div className='card_details-location'>Bangalore</div>
-                <div className='card_details-salaryRange'>Estimated Salary: &#8377;30 - &#8377;50 LPA </div>
+                <div className="card_details-companyName">{jobData_items.companyName}</div>
+                <div className='card_details-jobTitle'>{jobData_items.jobRole.toUpperCase()}</div>
+                <div className='card_details-location'>{jobData_items.location.charAt(0).toUpperCase() + jobData_items.location.slice(1)}</div>
+                <div className="card_details-salaryRange">
+                    Estimated Salary:{' '}
+                    {jobData_items.minJdSalary !== null && jobData_items.maxJdSalary !== null ? (
+                        `₹${jobData_items.minJdSalary} - ${jobData_items.maxJdSalary} LPA`
+                    ) : jobData_items.minJdSalary === null && jobData_items.maxJdSalary !== null ? (
+                        `Upto ₹${jobData_items.maxJdSalary} LPA`
+                    ) : (
+                        "Not Given"
+                    )}
+                </div>
                 <div className='card_details_aboutCompany'>
                     <p className='card_details_aboutCompany-heading'>About Company</p>
                     <div className="card_details_aboutCompany-description">
-                        This is a sample job and you must have displayed it to understand that its not just some random Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo eos ex ullam rem, nobis, deleniti enim consequuntur reiciendis facilis voluptatibus consectetur dolores est excepturi fuga dolor ipsa quidem doloribus pariatur.
+                        {jobData_items.jobDetailsFromCompany}
                     </div>
                 </div>
+
                 <div className="card_details_experience">
                     <div className="card_details_experience-heading">Minimum Experience</div>
-                    <div className="card_details_experience-years">6 years</div>
+                    <div className="card_details_experience-years">
+                        {
+                            jobData_items.minExp ?
+                                (` ${jobData_items.minExp} years`)
+                                : ("Not Given")
+                        }
+                    </div>
                 </div>
+
                 <button className='card_details-applyButton'>Easy Apply</button>
                 <button className='card_details-referralButton'>Unblock referral asks</button>
             </div>
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import JobCard from '../../components/jobCard/JobCard';
 import "./home.css"
 
@@ -8,6 +8,43 @@ import { roles_Options, employee_Options, experience_Options, work_Options, sala
 import FilterJob from '../../components/filterJob/FilterJob';
 
 const Home = () => {
+    const [jobData, setJobData] = useState([])
+
+    // fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions)
+    //     .then((response) => response.json())
+    //     .then((result) => console.log(result))
+    //     .catch((error) => console.error(error));
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            const body = JSON.stringify({
+                "limit": 10,
+                "offset": 0
+            });
+
+            const requestOptions = {
+                method: "POST",
+                headers: myHeaders,
+                body
+            };
+
+            try {
+                const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions)
+
+                const data = await response.json();
+                console.log(data.jdList)
+                setJobData(data.jdList);
+            }
+            catch (err) {
+                console.error(err);
+            }
+        }
+        fetchData();
+    }, [])
 
     return (
         <div className='wrapper'>
@@ -21,18 +58,10 @@ const Home = () => {
                     <FilterJob Options={location_Options} />
                 </div>
                 <div className="container_jobCard">
-                    <JobCard />
-                    <JobCard />
-                    <JobCard />
-                    <JobCard />
-                    <JobCard />
-                    <JobCard />
-                    <JobCard />
-                    <JobCard />
-                    <JobCard />
-                    <JobCard />
-                    <JobCard />
-                    <JobCard />
+                    {jobData?.map((jobData_items) => (
+                        <JobCard key={jobData_items.jdUid} jobData_items={jobData_items} />
+                    ))}
+
                 </div>
             </div>
         </div>
